3ca25921a3f2d198e0b096a833d5e169f10e33b6
Danil Gorchakov <Gorchakov@phystech.edu>
2017-10-13 00:03:55 +0300

    Server for mobile app

---
+++ src/main/java/com/qoobico/remindme/server/config/DatabaseConfig.java
@@ -0,0 +1,86 @@
+package com.qoobico.remindme.server.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+@Configuration
+@EnableJpaRepositories("com.qoobico.remindme.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("com.qoobico.remindme.server")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment environment;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        entityManager.setPackagesToScan(environment.getRequiredProperty("db.entity.package"));
+
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        entityManager.setJpaProperties(getHibernateProperties());
+        return entityManager;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driver"));
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+
+        dataSource.setInitialSize(Integer.valueOf(environment.getRequiredProperty("db.initialSize")));
+        dataSource.setMinIdle(Integer.valueOf(environment.getRequiredProperty("db.minIdle")));
+        dataSource.setMaxIdle(Integer.valueOf(environment.getRequiredProperty("db.maxIdle")));
+        dataSource.setTimeBetweenEvictionRunsMillis(Long.valueOf(environment.getRequiredProperty("db.timeBetweenEvictionRunsMillis")));
+        dataSource.setMinEvictableIdleTimeMillis(Long.valueOf(environment.getRequiredProperty("db.minEvictableIdleTimeMillis")));
+        dataSource.setTestOnBorrow(Boolean.valueOf(environment.getRequiredProperty("db.testOnBorrow")));
+        dataSource.setValidationQuery(environment.getRequiredProperty("db.validationQuery"));
+
+        return dataSource;
+    }
+
+    @Bean
+    PlatformTransactionManager transactionManager(){
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+        return manager;
+    }
+
+    public Properties getHibernateProperties(){
+        Properties properties = new Properties();
+        InputStream io = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+        try {
+            properties.load(io);
+            return properties;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can not find hibernate.properties in classpath ");
+        }
+    }
+
+}
